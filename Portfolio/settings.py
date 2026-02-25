40064a4b9d6ba3cb09eb763c8488335c2fc18a8e
Md Redwan Islam <redwanislamsiam13@gmail.com>
2026-02-25 13:48:06 +0600

    first commit

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tnb3b-3nj-(3)1bej!!)613-*43l-56l2i-=(xb*^osn$9i9te'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
